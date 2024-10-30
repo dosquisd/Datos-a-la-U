dc782bd1518d772ebdb67695e121c411b537cba7
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-10-30 10:26:28 -0500

    Modules added @nuxt/icon @nuxtjs/tailwindcss shadcn-vue for nuxt @nuxt/eslint

---
+++ nuxtapp/nuxt.config.ts
@@ -0,0 +1,16 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+    [
+      "shadcn-nuxt",
+      {
+        prefix: "",
+        componentDir: "./components/ui",
+      },
+    ],
+    "@nuxt/eslint",
+  ],
+});
