797c09a5bb6f8bed43468a0222ccca9b6448d641
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-10-30 20:46:34 -0500

    WHYYY THE DAMN DOCUMENTATION IS WRONG AAAAAAAAA *breathes*, the @nuxtjs/leaflet documentation says that out of the box it imports @vue-leaflet/vue-leaflet components Idk if Idk how to read but, WHYYYYYY is giving a 500 error code when copying the code example that is in the page IDK Just installing the package fixes the issue

--- nuxtapp/nuxt.config.ts
+++ nuxtapp/nuxt.config.ts
@@ -1,16 +1,17 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxt/icon",
-    "@nuxtjs/tailwindcss",
-    [
-      "shadcn-nuxt",
-      {
-        prefix: "",
-        componentDir: "./components/ui",
-      },
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+    modules: [
+        "@nuxt/icon",
+        "@nuxtjs/tailwindcss",
+        [
+            "shadcn-nuxt",
+            {
+                prefix: "",
+                componentDir: "./components/ui",
+            },
+        ],
+        "@nuxt/eslint",
+        "@nuxtjs/leaflet",
     ],
-    "@nuxt/eslint",
-  ],
 });
--- nuxtapp/pages/index.vue
+++ nuxtapp/pages/index.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+    import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+</script>
+
 <template>
-  <div>Hello There</div>
+    <ClientOnly>
+        <div class="w-screen h-screen">
+            <LMap
+                class="w-full h-full"
+                :zoom="6"
+                :center="[47.21322, -1.559482]"
+                :use-global-leaflet="false"
+            >
+                <LTileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    layer-type="base"
+                />
+            </LMap>
+        </div>
+    </ClientOnly>
 </template>
