1777b4ad7b8bc12db112c489bea203c62f35e544
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-10-31 21:29:50 -0500

    Tried to use pinia but it doesn't work Why? IDK Instead, just using nuxt composables :D

--- nuxtapp/pages/index.vue
+++ nuxtapp/pages/index.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
-    import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
     import type { GeoJsonObject } from "geojson";
-    import data from "@/data/data.json";
+    import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
+    import { z } from "zod";
+    import rawData from "@/data/data.json";
 
     useHead({
         title: "Home",
     });
+
+    const data = rawData as GeoJsonObject;
+
+    const DataShape = z.object({
+        layer: z.object({
+            feature: z.object({
+                properties: z.object({
+                    DPTO: z.string(),
+                    NOMBRE_DPT: z.string(),
+                }),
+            }),
+        }),
+    });
+
+    function handleClick(event: Event) {
+        const { department } = useOptions();
+
+        department.value =
+            DataShape.parse(event).layer.feature.properties.NOMBRE_DPT;
+    }
 </script>
 
 <template>
@@ -25,7 +46,7 @@
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     layer-type="base"
                 />
-                <LGeoJson :geojson="data as GeoJsonObject" />
+                <LGeoJson :geojson="data" @click="handleClick" />
             </LMap>
         </div>
     </ClientOnly>
