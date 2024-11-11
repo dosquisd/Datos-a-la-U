e099b99cc2c3f4fab75cde314a6f0af84368920d
JuanD <dosquisd123@gmail.com>
2024-11-11 14:58:01 -0500

    Add somes utils for API

---
+++ backend/api/utils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from datetime import date
+
+
+def dpto_to_cod(department: str, cods: dict) -> int:
+    return int(cods[department]['code'])
+
+
+def mpio_to_cod(municipality: str, department: str, cods: dict) -> int:
+    return int(cods[department]['municipalities'][municipality])
+
+
+def count(
+    df: pd.DataFrame,
+    min_date: date = None,
+    max_date: date = None
+) -> dict[str, list]:
+    df.index = df["IdSolicitud"]
+    count_df = df.groupby(by="FechaSolicitud").count()
+    count_df.rename(columns={"IdSolicitud": "Count"}, inplace=True)
+
+    # Limit dates
+    if min_date is not None:
+        count_df = count_df.loc[count_df.index >= min_date.strftime("%Y-%m-%d")]
+    if max_date is not None:
+        count_df = count_df.loc[count_df.index <= max_date.strftime("%Y-%m-%d")]
+
+    return {
+        "FechaSolicitud": [ts.strftime('%Y-%m-%d') for ts in count_df.index],
+        "Count": count_df["Count"].tolist()
+    }
