4d52bb7d070bd6899fc04d56367230f232741d44
Diederik A. Montaño <Diederiiik@gmail.com>
2024-11-12 00:16:00 -0500

    Data Analisis for models actualization
    No quiero ni escribir en inglés, hace sueño. Actualicé la gestión de los datos para los modelos de IA y empaqueté todo en funciones. Los quiero mucho, muchachones <3

--- backend/models/functions.py
+++ backend/models/functions.py
@@ -1,6 +1,6 @@
-""""
+"""
 Para evitar repetir continuamente algunas secciones de código, se crearán funciones en este
-archivo, para luego importalas y aplicarlas.
+archivo, para luego importarlas y aplicarlas.
 """
 
 import numpy as np
@@ -27,7 +27,6 @@ def divide_dataframe(df: pd.DataFrame, column: str) -> Dict:
 # Función para comprobar que el dataframe mayor y los subdatasets tienen la misma cantidad de elementos
 
 def compare_rows(df: pd.DataFrame, dfs: Dict) -> bool:
-
     dfRows = df.shape[0]
     dfsRows = 0
 
@@ -37,3 +36,45 @@ def compare_rows(df: pd.DataFrame, dfs: Dict) -> bool:
     print(f"({dfRows}, {dfsRows})")
 
     return True if dfRows == dfsRows else False
+
+
+# Función para llenar los espacios vacíos en los días de un dataframe
+
+def create_continuos_time_series(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Esta función crea un arreglo con una fila por cada día entre la primera y última fecha de un
+    dataframe, sin tener en cuenta los días sábados y domingos (En los que no abren las casas de justicia)
+
+    Dicho arreglo contiene un conteo de los casos abiertos por día, en lugar de una fila por cada caso abierto.
+
+    Como entrada se recibe un dataframe basado en 'backend/data/processed/unified_data.csv',
+    por lo que se espera tener la columna 'FechaSolicitud'.
+    """
+
+    # Luego creamos el dataframe con solo los días laborales
+    continuos_days = pd.date_range(start=df['FechaSolicitud'].min(), end=df['FechaSolicitud'].max(), freq='B')
+    df_continuous_days = pd.DataFrame({'FechaSolicitud': continuos_days})
+
+    # Unificamos los dataframes
+    fulled_df = pd.merge(df_continuous_days, df, on='FechaSolicitud', how='left')
+
+    # Rellenamos los valores faltantes con ceros
+    fulled_df.fillna(0, inplace=True)
+
+    # Agrupamos por fechas
+    final_df = fulled_df.groupby('FechaSolicitud').size().reset_index(name='CantidadCasos')
+
+    """
+    En este punto existe un problema, y es que al realizar la unión de los dataframes hay
+    al menos una fila por cada día, así que el valor más bajo de "Cantidad Casos" es 1.
+    
+    Aún así, por la forma en la que se realiza el merge de los dataframes, este aumento en 1
+    solo sucede en las filas que tenía df_continuous_days y no el df original.
+    
+    Buscaremos las fechas de final_df que no están en el df original y enviaremos su cantidad
+    de casos a 0
+    
+    """
+    final_df.loc[~final_df['FechaSolicitud'].isin(df['FechaSolicitud']), 'CantidadCasos'] = 0
+
+    return final_df
